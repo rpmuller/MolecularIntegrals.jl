f5a1f502db3e297286637f148ff92101cfd98633
Richard P. Muller <rmuller@sandia.gov>
2021-04-03 17:10:48 -0600

    Added runtest and basis testset

---
+++ test/runtest.jl
@@ -0,0 +1,15 @@
+using MolecularIntegrals, Test
+
+@testset "Basis" begin
+    s = pgbf(1.0)
+    px = pgbf(1.0,0,0,0,1,0,0)
+    @test amplitude(s,0,0,0) ≈ 0.71270547
+    @test amplitude(px,0,0,0) ≈ 0
+    c = cgbf(0.0,0.0,0.0)
+    push!(c,1,1)
+    @test amplitude(c,0,0,0) ≈ 0.71270547
+    c2 = cgbf(0,0,0)
+    push!(c2,1,0.2)
+    push!(c2,0.5,0.2)
+    @test overlap(c2,c2) ≈ 1
+end
