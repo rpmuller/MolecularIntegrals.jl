56a6ed5592cd286fc7a6a19c7abb9c17eb6cbec1
Richard P. Muller <rmuller@sandia.gov>
2021-04-06 19:55:12 -0600

    More playing with timings. No real progress.

--- test/timing.jl
+++ test/timing.jl
@@ -2,5 +2,5 @@ using MolecularIntegrals, BenchmarkTools
 
 
 bfs = build_basis(ethane,"6-31G")
-@btime MolecularIntegrals.all_twoe_ints(bfs);
+@time MolecularIntegrals.all_twoe_ints(bfs);
 #@time MolecularIntegrals.all_twoe_ints(bfs,MolecularIntegrals.coulomb_hgp);
