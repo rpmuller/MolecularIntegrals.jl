97e344e34251ed5d13494f725c91421a36be54a3
Richard P. Muller <rmuller@sandia.gov>
2021-04-17 09:40:27 -0600

    Wrote/compiled vrr3 code using arrays

--- src/HGP2.jl
+++ src/HGP2.jl
@@ -46,6 +46,134 @@
 "prunem - Keep only the dictionary keys with m (last index) = 0"
 prunem(d::Dict) = Dict(k[1:end-1] => v for (k,v) in d if k[end] == 0)
 
+"contracted_vrr - compute and contract the vertical recurrance relations 
+                between shells ash,bsh,csh,dsh
+"
+function contracted_vrr(ash,bsh,csh,dsh)
+    values = Dict()
+    amax,cmax = ash.L+bsh.L,csh.L,dsh.L
+    A,B,C,D = ash.xyz,bsh.xyz,csh.xyz,dsh.xyz
+    for (aexpn,acoef) in zip(ash.expns,ash.coefs)
+        for (bexpn,bcoef) in zip(bsh.expns,bsh.coefs)
+            for (cexpn,ccoef) in zip(csh.expns,csh.coefs)
+                for (dexpn,dcoef) in zip(dsh.expns,dsh.coefs)
+                    # This is where the advantage of sparse arrays over dicts becomes evident.
+                    #  we want to just do values += acoef*bcoef*ccoef*dcoef*varray
+                    varray = vrr2(amax,cmax, aexpn,bexpn,cexpn,dexpn,A,B,C,D)
+                end
+            end
+        end
+    end
+    return values
+end
+
+"vrr3 - version of vrr2 that uses arrays instead of dicts"
+function vrr3(amax,cmax, aexpn,bexpn,cexpn,dexpn, A,B,C,D)
+    # TODO: get results to match vrr2
+    # TODO: use sparse arrays
+    mmax=amax+cmax
+    values = OffsetArray(zeros(Float64,amax+1,amax+1,amax+1,cmax+1,cmax+1,cmax+1,mmax+1),
+         0:amax,0:amax,0:amax, 0:cmax,0:cmax,0:cmax,0:mmax) 
+    P = gaussian_product_center(aexpn,A,bexpn,B)
+    Q = gaussian_product_center(cexpn,C,dexpn,D)
+    zeta,eta = aexpn+bexpn,cexpn+dexpn
+    ze = zeta+eta
+    W = gaussian_product_center(zeta,P,eta,Q)
+    rab2 = dist2(A-B)
+    rcd2 = dist2(C-D)
+    rpq2 = dist2(P-Q)
+    T = zeta*eta*rpq2/ze
+    Kab = sqrt(2)pi^1.25/zeta*exp(-aexpn*bexpn*rab2/zeta)
+    Kcd = sqrt(2)pi^1.25/eta*exp(-cexpn*dexpn*rcd2/eta)
+    
+    # HGP equation 6, with b=d=0:
+    #   [a+1,c]m = (Pi-Ai)[a,c]m + (Wi-Pi)[a,c]m+1 
+    #        + a_i/2zeta ([a-1,c]m - eta/zeta+eta[a-1,c]m+1)        # eq 6a
+    #        + ci/2(zeta+eta)[a,c-1]m+1
+
+    # First generate (0,0,0, 0,0,0, m) using eq 12
+    for m in 0:mmax
+        values[0,0,0, 0,0,0, m] = Kab*Kcd*Fgamma(m,T)/sqrt(ze)
+    end
+
+    # Now generate (ax,ay,az,0,0,0,m) 
+    # Eq 6a, with c=0 also:
+    #   [a+1,0]m = (Pi-Ai)[a,0]m + (Wi-Pi)[a,0]m+1 
+    #        + a_i/2zeta ([a-1,0]m - eta/zeta+eta[a-1,0]m+1)        # eq 6b
+    for ashell in 1:amax
+        for ap in shell_indices[ashell]
+            apx,apy,apz = ap
+            i = argmax(ap) # Choose argmax(ap) as the direction to use for building new terms
+            a = vdiff(ap,i,-1)
+            am = vdiff(ap,i,-2)
+            ax,ay,az = a
+            amx,amy,amz = am
+            for m in 0:(mmax-ashell)
+                values[apx,apy,apz,0,0,0,m] = (P[i]-A[i])*values[ax,ay,az,0,0,0,m]+(W[i]-P[i])*values[ax,ay,az,0,0,0,m+1]
+                if am[i] >= 0
+                    values[apx,apy,apz,0,0,0,m] += a[i]/(2*zeta)*(values[amx,amy,amz,0,0,0,m]-eta/ze*values[amx,amy,amz,0,0,0,m+1])
+                end
+            end
+        end
+    end
+
+    # Next build (0,0,0,cx,cy,cz,m)
+    # The c-based version of 6a is:
+    #   [0,c+1]m = (Qi-Bi)[0,c]m + (Wi-Qi)[0,c]m+1
+    #       + ci/2eta ([0,c-1]m - zeta/zeta+eta[0,c-1]m+1)         # eq 6c
+    # 
+    for cshell in 1:cmax
+        for cp in shell_indices[cshell]
+            cpx,cpy,cpz = cp
+            i = argmax(cp)  # Choose argmax(cp) as the direction to use for building new terms
+            c = vdiff(cp,i,-1)
+            cm = vdiff(cp,i,-2)
+            cx,cy,cz = c
+            cmx,cmy,cmz = cm
+            for m in 0:(mmax-cshell)
+                values[0,0,0,cpx,cpy,cpz,m] = (Q[i]-C[i])*values[0,0,0,cx,cy,cz,m]+(W[i]-Q[i])*values[0,0,0,cx,cy,cz,m+1]
+                if cm[i] >= 0
+                    values[0,0,0,cpx,cpy,cpz,m] += c[i]/(2*eta)*(values[0,0,0,cmx,cmy,cmz,m]-zeta/ze*values[0,0,0,cmx,cmy,cmz,m+1])
+                end
+            end
+        end
+    end
+
+    # Now build (ax,ay,az,cx,cy,cz,m)
+    # The c-based version of 6a is:
+    #   [a,c+1]m = (Qj-Bi)[a,c]m + (Wj-Qj)[a,c]m+1
+    #       + c_j/2eta ([a,c-1]m - zeta/zeta+eta[a,c-1]m+1)         # eq 6d
+    #       + a_j/2(zeta+eta)[a-1,c]m+1
+    for ashell in 1:amax
+        for a in shell_indices[ashell]
+            ax,ay,az = a
+            for cshell in 1:cmax
+                for cp in shell_indices[cshell]
+                    cpx,cpy,cpz = cp
+                    j = argmax(cp)  # Choose argmax(cp) as the direction to use for building new terms
+                    c = vdiff(cp,j,-1)
+                    cm = vdiff(cp,j,-2)
+                    cx,cy,cz = c
+                    cmx,cmy,cmz = cm
+
+                    am = vdiff(a,j,-1)
+                    amx,amy,amz = am
+                    for m in 0:(mmax-ashell-cshell)
+                        values[ax,ay,az,cpx,cpy,cpz,m] = (Q[j]-C[j])*values[ax,ay,az,cx,cy,cz,m]+(W[j]-Q[j])*values[ax,ay,az,cx,cy,cz,m+1]
+                        if cm[j] >= 0
+                            values[ax,ay,az,cpx,cpy,cpz,m] += c[j]/(2*eta)*(values[ax,ay,az,cmx,cmy,cmz,m]-zeta/ze*values[ax,ay,az,cmx,cmy,cmz,m+1])
+                        end
+                        if am[j] >= 0 
+                            values[ax,ay,az,cpx,cpy,cpz,m] += a[j]/(2*ze)*(values[amx,amy,amz,cx,cy,cz,m+1])
+                        end                             
+                    end
+                end
+            end
+        end 
+    end
+    return values[:,:,:,:,:,:,0]
+end
+
 "vrr2 - iterative version of HGP vertical recurrance relations"
 function vrr2(amax,cmax, aexpn,bexpn,cexpn,dexpn, A,B,C,D)
     values = Dict()
