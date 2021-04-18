3ddcc95a933211f8e40f50b1bfc50d20f6844e4d
Rick Muller <rpmuller@gmail.com>
2021-04-17 19:41:24 -0600

    Cleaning, updating notes.

--- TODO.md
+++ TODO.md
@@ -1,24 +1,16 @@
 # TODO and notes
 
-## Reconcile coulomb(px,s,s,s) with hrr2 calls: 
+## Figure out method to generate integral records
+May have to generate an indexing array that points to the
+different shells, primitive, and contracted functions.
+
+## Reconcile coulomb(px,s,s,s) with hrr calls: 
 While working on the new vrr code psss(), I found a discrepancy comparing to coulomb 
 that I originally assumed was a mistake in psss(), but which I later found matched
 vrr for this code. Which means that it is likely that the following test fails:
 @test MolecularIntegrals.vrr(1.0,0,0,0,1,0,0,1.0,0,0,0,1.0,0,0,0,0,0,0,1.0,0,0,0,0) ≈ coulomb(px,s,s,s)
 I'm going to move forward with coding the vrr routines, but I'm flagging this as
 something to investigate and fix later.
-
-## Improve data structures used for ints in vrr2 and hrr2:
-Note on the interface: currently the new vrr routines are returning multidimensional dictionary
-arrays over angular momentum states and m. If mmax=0 is submitted, this will reduce the indices in return by 1,
-allowing, for example, a 3x1 maxtrix to be accessed as a 3 vector. Could cause problems with higher dimension
-returns (3x3x1, e.g.)
-- Such a data structure could be using the ao l,m indices to map the I,J,K. There's preliminary code in Basis.jl
-- Could also use a SparseArray
-
-## Include lists of primitive exponents to contract vrr2 results
-The ultimate new vrr code will return a [ix,iy,iz,jx,jy,jz] matrix. Each term of this will be a primitive 
-integral. We will need to find a way to do the contraction over multiple primitives, potentially introducing another index that we'll contract over: [k,ix,iy,iz,jx,jy,jz]. Although an 8-dimensional array is large, none of the dimensions will have very many terms. Still 4^8 is a lot of terms. I wonder whether this will require a more intelligent data structure.
 
 ## Only computing, returning symmetric ERI pairs (i>j,k>l) ij>kl
 Think about ordering an integral call (ij,kl) such that i>j, k>l, and ij>kl.
--- test/timing.jl
+++ test/timing.jl
@@ -18,11 +18,11 @@ function vrr_timings()
     cx,cy,cz = dx,dy,dz = xyza
     aexpn=bexpn=cexpn=dexpn = ex
     @time MolecularIntegrals.vrr2(2,2, ex,ex,ex,ex, xyz,xyz,xyza,xyza);
-    @time MolecularIntegrals.vrr3(2,2, ex,ex,ex,ex, xyz,xyz,xyza,xyza);
+    @time MolecularIntegrals.vrr(2,2, ex,ex,ex,ex, xyz,xyz,xyza,xyza);
 end
 # Results for above case:
 # vrr2 0.390068 seconds (791.39 k allocations: 45.704 MiB, 3.79% gc time, 99.51% compilation time)
-# vrr3 0.014459 seconds (6.29 k allocations: 289.375 KiB, 97.21% compilation time)
+# vrr  0.014459 seconds (6.29 k allocations: 289.375 KiB, 97.21% compilation time)
 
 function hrr_timings()
     x=y=z=0.0
@@ -35,15 +35,19 @@ function hrr_timings()
     C = D = xyza
     cx,cy,cz = dx,dy,dz = xyza
     aexpn=bexpn=cexpn=dexpn = ex
-    ashell,bshell,cshell,dshell = (2,1,2,1)
-    #@time MolecularIntegrals.hrr(aexpn,ax,ay,az,aI,aJ,aK, bexpn,bx,by,bz,bI,bJ,bK, cexpn,cx,cy,cz,cI,cJ,cK, dexpn,dx,dy,dz,dI,dJ,dK);
+    ashell,bshell,cshell,dshell = (2,2,2,2)
+    #@time MolecularIntegrals.hrr_r(aexpn,ax,ay,az,aI,aJ,aK, bexpn,bx,by,bz,bI,bJ,bK, cexpn,cx,cy,cz,cI,cJ,cK, dexpn,dx,dy,dz,dI,dJ,dK);
     @time MolecularIntegrals.hrr2(ashell,bshell,cshell,dshell, aexpn,bexpn,cexpn,dexpn, A,B,C,D);
     @time MolecularIntegrals.hrr3(ashell,bshell,cshell,dshell, aexpn,bexpn,cexpn,dexpn, A,B,C,D);
-    @time MolecularIntegrals.hrr4(ashell,bshell,cshell,dshell, aexpn,bexpn,cexpn,dexpn, A,B,C,D);
+    @time MolecularIntegrals.hrr(ashell,bshell,cshell,dshell, aexpn,bexpn,cexpn,dexpn, A,B,C,D);
 end
 # Results for hrr, shells=2,1,2,1, xyz, xyza, ex as above:
 # hrr2: 0.677294 seconds (1.03 M allocations: 58.055 MiB, 14.63% gc time, 98.78% compilation time)
 # hrr3: 0.356590 seconds (835.99 k allocations: 45.983 MiB, 2.83% gc time, 98.99% compilation time)
-# hrr4: 0.003234 seconds (44.50 k allocations: 2.051 MiB)
+# hrr:  0.003234 seconds (44.50 k allocations: 2.051 MiB)
+# Results for hrr, shells = 2,2,2,2:
+# hrr2 0.647392 seconds (1.26 M allocations: 68.603 MiB, 10.92% gc time, 94.79% compilation time)
+# hrr3 0.425849 seconds (981.26 k allocations: 136.233 MiB, 4.71% gc time, 81.78% compilation time)
+# hrr  0.017597 seconds (221.12 k allocations: 10.490 MiB)
 
-vrr_timings()
+hrr_timings()
