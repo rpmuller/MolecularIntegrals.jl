e10b9ff02be8a0bf5ff530dbff41e19fef43e53e
Rick Muller <rpmuller@gmail.com>
2021-04-21 15:37:41 -0600

    Wrote chrr

--- TODO.md
+++ TODO.md
@@ -2,19 +2,16 @@
 
 ## New strategy for vrr/hrr:
 1. Optimize loop structure for vrr/hrr
-[X] Write special purpose routines for ss,sp,ps,pp,; only call general 
-code when you exceed one of these cases, but make sure the general case still works
-for all of these.
-2. Test special purpose routines
-3. Create more structured testing of integral code using coulomb(), which should work now.
-Consider something more like fuzz testing.
-5. Write out a standard integral record using this code
-6. Figure out metaprogramming to generate code for the special cases.
+2. Time special purpose routines
+3. Test special purpose routines
+
 7. Decide on a better m2ao: are the arguments arrays or tuples. Are they both?
 
 ## chrr
-- Finish chrr
-- Figure out a way to share code between chrr and hrr
+- Test chrr
+
+## Write out a standard integral record using this code
+
 
 ## Only computing, returning symmetric ERI pairs (i>j,k>l) ij>kl
 Think about ordering an integral call (ij,kl) such that i>j, k>l, and ij>kl.
@@ -23,6 +20,9 @@ Also, put in warning when bsh>ash or dsh>csh in hrr2?
 # Someday changes
 ## Make bf IJK and amplitude() calls to arrays
 ## Write/test special purpose code for VRRs: ds,sd,dp,pd,dd
+## Create more structured testing of integral code using coulomb(), which should work now.
+- Consider something more like fuzz testing.
+## Figure out metaprogramming to generate code for the special hand-codedcases.
 
 ## Implement other molecule methods:
 - nocc, nclosed, nopen, nup, ndown, stoich, center!
--- src/HGP.jl
+++ src/HGP.jl
@@ -58,8 +58,7 @@ export vrr,cvrr,hrr,chrr
 "
 function cvrr(ash::Shell,bsh::Shell,csh::Shell,dsh::Shell)
     amax,cmax = ash.L+bsh.L,csh.L,dsh.L
-    cvrrs = OffsetArray(zeros(Float64,amax+1,amax+1,amax+1,cmax+1,cmax+1,cmax+1),
-         0:amax,0:amax,0:amax, 0:cmax,0:cmax,0:cmax) 
+    cvrrs = zeros(Float64,nao(amax),nao(cmax))
     A,B,C,D = ash.xyz,bsh.xyz,csh.xyz,dsh.xyz
     for (aexpn,acoef) in zip(ash.expns,ash.coefs)
         for (bexpn,bcoef) in zip(bsh.expns,bsh.coefs)
@@ -84,11 +83,69 @@ function unit(n,d)
 end
 
 function chrr(ash::Shell,bsh::Shell,csh::Shell,dsh::Shell)
-    # There must be ways to reuse space from hrr().
+    # There must be ways to reuse code from hrr().
+    ao2m,m2ao = ao_arrays()
+    ashell,bshell,cshell,dshell = ash.L,bsh.L,csh.L,dsh.L
+    A,B,C,D = ash.xyz,bsh.xyz,csh.xyz,dsh.xyz
     vrrs = cvrr(ash,bsh,csh,dsh) 
-    hrrs = Dict{NTuple{12,Int},Float64}() # could also use DefaultDict(0)    
-    # Finish!
-    return hrrs
+    hrrs = zeros(Float64,nao(ashell+bshell),nao(bshell),nao(cshell+dshell),nao(dshell))
+    hrrs[:,1,:,1] = vrrs[:,:]
+
+    # First build (ab,c0) from (a0,c0)
+    for bs in 1:bshell 
+        for bp in shell_indices[bs]
+            bpindex = m2ao[bp]
+            j = argmax(bp)
+            b = vdiff(bp,j,-1)
+            bindex = m2ao[b]
+            for as in 0:(ashell+bshell-bs)
+                for a in shell_indices[as]
+                    aindex = m2ao[a]
+                    ap = vdiff(a,j,1)
+                    apindex = m2ao[ap]
+                    for cs in 0:(cshell+dshell)
+                        for c in shell_indices[cs]
+                            cindex = m2ao[c]
+                            hrrs[aindex,bpindex,cindex,1] = hrrs[apindex,bindex,cindex,1] + 
+                                (A[j]-B[j])*hrrs[aindex,bindex,cindex,1]
+                        end
+                    end
+                end
+            end
+        end
+    end
+    # now build (ab,cd) from (ab,c0)
+    for ds in 1:dshell
+        for dp in shell_indices[ds]
+            dpindex = m2ao[dp]
+            j = argmax(dp)
+            d = vdiff(dp,j,-1)
+            dindex = m2ao[d]
+            for cs in 0:(cshell+dshell-ds) 
+                for c in shell_indices[cs]
+                    cindex = m2ao[c]
+                    cp = vdiff(c,j,1)
+                    cpindex = m2ao[cp]
+                    for as in 0:ashell
+                        for a in shell_indices[as]
+                            aindex = m2ao[a]
+                            for bs in 0:bshell
+                                for b in shell_indices[bs]
+                                    bindex = m2ao[b]
+                                    hrrs[aindex,bindex,cindex,dpindex] = hrrs[aindex,bindex,cpindex,dindex] +
+                                        (C[j]-D[j])*hrrs[aindex,bindex,cindex,dindex]
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+
+
+    return hrrs[1:nao(ash),:,1:nao(csh),:]
 end
 
 function vrr_ss(aexpn,bexpn,cexpn,dexpn, A,B,C,D)
