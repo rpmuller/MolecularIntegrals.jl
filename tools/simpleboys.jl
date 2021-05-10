81fe745107f2ff1bdf5d3d530bcd95a08cfed2ca
Rick Muller <rpmuller@gmail.com>
2021-05-09 20:56:47 -0600

    Updated boys notes

--- tools/simpleboys.jl
+++ tools/simpleboys.jl
@@ -15,18 +15,21 @@ md"""
 # Simple questions and simple answers about the Boys function
 Rick Muller
 
+## TODOs
+- [ ] Redo this with the special function version of the Boys function.
+
 A large part of electronic structure theory lies in computing the repulsive
 interaction between electrons, and a large part of electron repulsion 
 is captured in the Boys function that computes the repulsion between
 partially overlapping electronic wave functions.
 
-The Boys function is given by [^boys]
+The Boys function is given by [^boys50]
 
 $Fm(T)=\int_0^1 u^{2m}\exp(-Tu^2)du$
 
-and was introduced by Boys in 196?. Since that time, a surprising amount
-of work has gone into efficiently computing [^gjp] this function. The
-best of these methods [^gjp] divides the integration over $u$ into multiple slices
+and was introduced by Boys in 1950. Since that time, a surprising amount
+of work has gone into efficiently computing [^gjp91] this function. The
+best of these methods [^gjp91] divides the integration over $u$ into multiple slices
 and approximates the integrand in this region using Taylor or Chebyshev
 polynomials. Efficient programs [^libint] to evaluate these terms have detailed
 tables with nodes and weights for the interpolation and quadrature schemes.
@@ -119,7 +122,7 @@ it is essentially parameter-free when `a=1`, and thus represents a very efficien
 # ╔═╡ 4dd0b1e6-61f0-4daa-80f1-625dc793b33a
 md"""
 As an aside, we certainly don't mean to denigrate the excellent work that has
-gone into the development of the approximations methods [^gjp] or their 
+gone into the development of the approximations methods [^gjp91] or their 
 efficient implementation [^libint]. These are remarkably accurate and fast
 methods that have been derived with great thought and applied repeatedly to
 chemical calculations with excellent results. Our intent here is to merely
@@ -148,21 +151,8 @@ end;
 begin
 	rs2=0:0.1:15
 	plot(rs2,[fboys(0,r) for r in rs2],label="fboys(0,r)")
-	plot!(rs2,[fapprox2(r,1,1) for r in rs2],label="fapprox2(r,a=1,b=1.5)")
+	plot!(rs2,[fapprox2(r,1,1.5) for r in rs2],label="fapprox2(r,a=1,b=1.5)")
 	#title!("Parameter-free approximation of Boys function")
-end
-
-# ╔═╡ 26bfe04a-99bd-4089-a9fa-609abd30659b
-md"""
-This is accurate enough that it's worth just plotting the difference:
-"""
-
-# ╔═╡ 4c4c7b38-49a1-4615-a988-7acf5b715d71
-begin
-	b = 1
-	plot(rs2,[fboys(0,r)-fapprox2(r,1,b) for r in rs2],label="Error(r)")
-	err = round(maximum([abs(fboys(0,r)-fapprox2(r,1,b)) for r in rs2]),digits=3)
-	title!("Max error = $err")
 end
 
 # ╔═╡ 8c8adf9b-5eff-4942-9a49-95a4aa1e2002
@@ -174,18 +164,15 @@ the decay of the constant term, what would that function look like?
 """
 
 # ╔═╡ 9428670f-5cf7-4c9e-bb93-dd0747a34182
-boysdenom(T) = log(1/fboys(0,T) - sqrt(2T)/sqrt(pi/2));
-
-# ╔═╡ 1ff6a167-aa99-4a64-8621-ba9cee33cdcd
-approxdenom(T) = -T-0.5;
-
-# ╔═╡ 0dfe990a-afa9-4c33-bf5d-3a655f35a844
-md"It turns out the function looks very much like $\exp(-T-0.5)$, apart from a little cusp."
+boysdenom(T) = 1/fboys(0,T) - sqrt(2T)/sqrt(pi/2);
 
 # ╔═╡ 771d4eeb-72e6-4863-a114-ff203a42455b
 begin
-	plot(rs2,boysdenom.(rs2),label="Boys denom log decay")
-	plot!(rs2,approxdenom.(rs2),label="approx log")
+	rs3 = 0:0.1:3
+	plot(rs3,boysdenom.(rs3),label="Boys denom decay")
+	plot!(rs3,[exp(-T-0.56) for T in rs3],label="-T-0.56")
+	plot!(rs3,[exp(-4T) for T in rs3],label="-4T")
+	#plot!(rs3,[exp(-4T)/2+exp(-T-0.56)/2 for T in rs3],label="avg")
 end
 
 # ╔═╡ 997e6018-bb0e-4933-8b13-b904e96fc819
@@ -194,35 +181,75 @@ md"Plot this as `fapprox3`"
 # ╔═╡ 894135da-1ef9-4170-aa49-1b6307e398eb
 function fapprox3(T,a=1)
 	pre = sqrt(0.5*pi)
-	return pre/(a*pre*exp(-T-0.5) + (2T)^(0.5))
+	return pre/(a*pre*exp(-T-0.56) + (2T)^(0.5))
 end;
 
 # ╔═╡ 9dfb09bb-22d8-44f2-9c4b-98777e43de64
 begin
-	plot(rs2,[fboys(0,r) for r in rs2],label="fboys(0,r)")
-	plot!(rs2,[fapprox3(r) for r in rs2],label="fapprox3(r)")
+	plot(rs3,[fboys(0,r) for r in rs3],label="fboys(0,r)")
+	plot!(rs3,[fapprox2(r) for r in rs3],label="fapprox2(r)")
+	plot!(rs3,[fapprox3(r) for r in rs3],label="fapprox3(r)")
 end
 
-# ╔═╡ e530c663-afb5-46b8-a645-bd1a4eede764
-md"Plot the difference close to the origin:"
+# ╔═╡ d526caa1-ff7e-4ec5-ab59-2a10e9ad9c5e
+md"""
+## Plot of multiple Fm(T) values
 
-# ╔═╡ 262cee5f-cdcb-4505-aee5-07077c815ffb
+Let's generalize this process a bit. 
+"""
+
+# ╔═╡ 3923126f-93be-48f2-bd37-3ef50588ff47
 begin
-	r3 = 0:0.1:5
-	plot(r3,[fboys(0,r)-fapprox3(r) for r in r3],label="Error approx3")
+	plot(rs,fboys.(0,rs))
+	plot!(rs,fboys.(1,rs))
+	plot!(rs,fboys.(2,rs))
+	plot!(rs,fboys.(3,rs))
+	plot!(rs,fboys.(4,rs))
+	plot!(rs,fboys.(5,rs))
+	plot!(rs,fboys.(6,rs))
 end
+
+# ╔═╡ eb05a798-8eb2-4ff1-b88f-b58babe64891
+boysdecay(T,m) = prod((2m-1):-2:1)/fboys(m,T) - (2T)^(m+0.5)/sqrt(pi/2);
+
+# ╔═╡ 7023c918-04ee-45da-8f5d-daca68ba0930
+begin
+	plot(rs,boysdecay.(rs,0))
+	plot!(rs,boysdecay.(rs,1))
+	plot!(rs,boysdecay.(rs,2))
+	plot!(rs,boysdecay.(rs,3))
+	plot!(rs,boysdecay.(rs,4))
+	plot!(rs,boysdecay.(rs,5))
+	plot!(rs,boysdecay.(rs,6))
+	plot!(rs,boysdecay.(rs,7))
+	plot!(rs,boysdecay.(rs,8))
+	plot!(rs,boysdecay.(rs,9))
+end
+	
+
+# ╔═╡ bbac1813-8a6f-4984-828a-a51cc577f730
+md"""
+I can certainly fit this to a spline, but I feel like I'm missing a term or two. Part of the problem is that since `fboys` goes to zero so quickly for `m>1`, the residue
+becomes large quickly.
+"""
+
+# ╔═╡ 022cc892-63d4-4c01-9b55-66017d1d90b8
+md"On the other hand, we could always just interpolate the Boys function directly.
+I guess the advantage of interpolating the decay function is that once it's zero,
+it doesn't really matter how much faster it decays."
 
 # ╔═╡ 44107e8f-116c-475f-b6d4-2b5aecc2f1e8
-md"## References
-[^boys]: Boys 1961?
-[^gjp]: Gill, Johnson, Pople 1991.
-[^libint]: Libint
-[^mn57]: K. Nishimoto, N. Mataga. *Z. Phys.*, 12, 335-338 (1957).
-"
+md"""
+## References
+[^boys50]: S.F. Boys. "Electronic Wave Functions. I. A General Method of Calculation for the Stationary States of Any Molecular System." *Proc. R. Soc. Lond.*, A200, 542-554 (1950).
+[^gjp91]: P.M.W. Gill, B.G. Johnson, "Two-Electron Integrals Over Gaussian *s* Functions". J.A. Pople. *IJQC*. 40, 745-752, 1991.
+[^libint]: E.F. Valeev. "Libint: A library for the evaluation of molecular integrals of many-body operators over Gaussian functions." [http://libint.valeyev.net/](http://libint.valeyev.net/)
+[^mn57]: K. Nishimoto, N. Mataga. "Electronic Structure and Spectra of Some Nitrogen Heterocycles." *Z. Phys.*, 12, 335-338 (1957).
+"""
 
 # ╔═╡ Cell order:
 # ╟─6b21238e-f8de-4c77-9c86-9dbd4691b6f5
-# ╟─4e16dbbe-b0fc-11eb-2349-ad7ccd0ff56b
+# ╠═4e16dbbe-b0fc-11eb-2349-ad7ccd0ff56b
 # ╠═8a736133-17f2-446d-9ab5-4bc46bdd556b
 # ╟─bf014265-210c-412f-9af4-5a654f557de9
 # ╟─d8061039-367c-42ee-8a65-a32dca37fc67
@@ -237,16 +264,16 @@ md"## References
 # ╟─fdd2691f-f26f-4c26-8cc6-0d8edaa5278a
 # ╠═a0a09c9f-c6d2-422f-b32a-8e3f8131b3c3
 # ╠═ae9658a3-9241-423d-9daa-d6644e8a206a
-# ╟─26bfe04a-99bd-4089-a9fa-609abd30659b
-# ╠═4c4c7b38-49a1-4615-a988-7acf5b715d71
-# ╠═8c8adf9b-5eff-4942-9a49-95a4aa1e2002
+# ╟─8c8adf9b-5eff-4942-9a49-95a4aa1e2002
 # ╠═9428670f-5cf7-4c9e-bb93-dd0747a34182
-# ╠═1ff6a167-aa99-4a64-8621-ba9cee33cdcd
-# ╟─0dfe990a-afa9-4c33-bf5d-3a655f35a844
 # ╠═771d4eeb-72e6-4863-a114-ff203a42455b
 # ╟─997e6018-bb0e-4933-8b13-b904e96fc819
 # ╠═894135da-1ef9-4170-aa49-1b6307e398eb
 # ╠═9dfb09bb-22d8-44f2-9c4b-98777e43de64
-# ╠═e530c663-afb5-46b8-a645-bd1a4eede764
-# ╠═262cee5f-cdcb-4505-aee5-07077c815ffb
+# ╠═d526caa1-ff7e-4ec5-ab59-2a10e9ad9c5e
+# ╠═3923126f-93be-48f2-bd37-3ef50588ff47
+# ╠═eb05a798-8eb2-4ff1-b88f-b58babe64891
+# ╠═7023c918-04ee-45da-8f5d-daca68ba0930
+# ╠═bbac1813-8a6f-4984-828a-a51cc577f730
+# ╠═022cc892-63d4-4c01-9b55-66017d1d90b8
 # ╠═44107e8f-116c-475f-b6d4-2b5aecc2f1e8
