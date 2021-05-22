92578c40653bc3d2e493a617b08da4892f0676c9
Rick Muller <rpmuller@gmail.com>
2021-05-22 17:40:01 -0600

    updated with today's timings

--- tools/timing-history.jl
+++ tools/timing-history.jl
@@ -13,13 +13,13 @@ using Dates
 # ╔═╡ 1196b181-459a-47e8-b57e-f320b0471a89
 begin
 	dates = ["4/6/2021","4/24/2021","4/25/2021","4/26/2021","4/27/2021",
-		"4/28/2021","4/30/2021","5/1/2021","5/7/2021","5/8/2021"]
-	days = [1,18,19,20,21,22,24,25,31,32]
-	seconds = [44.1,22,7.83,2.12,2.04,1.11,0.938,0.831,0.631,0.535]
+		"4/28/2021","4/30/2021","5/1/2021","5/7/2021","5/8/2021","5/22/2021"]
+	days = [1,18,19,20,21,22,24,25,31,32,46]
+	seconds = [44.1,22,7.83,2.12,2.04,1.11,0.938,0.831,0.631,0.535,0.366]
 end
 
 # ╔═╡ 9b3d18a3-a176-4402-874c-45f15d1f6e14
-plot(days,seconds,yaxis=:log,xlabel="Days",ylabel="Seconds",title="Benchmark speed for Ethane/6-31G Integrals",label=false)
+plot(days,seconds,xlabel="Days",ylabel="Seconds",title="Benchmark speed for Ethane/6-31G Integrals",label=false)
 
 # ╔═╡ 59ebedbe-b0c9-439b-bec6-a0aa094e6c84
 md"Here's how to do this using dates instead of days. You need to define a 
